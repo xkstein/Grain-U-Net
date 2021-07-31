bfcb408f03f5fff9c7b357ac65f9551a102c0915
javierlopez6466 <57878089+javierlopez6466@users.noreply.github.com>
2021-07-31 00:14:38 -0400

    Add files via upload
    This works fine in a cell in google colab but once I try to import it and call it does not work. Will try to see why.

---
+++ randerr.py
@@ -0,0 +1,30 @@
+'''
+This file contains the necessary functions to find the rand err
+between two binarized skeletonized images of the same size.
+Simply perform:
+
+err = randerr(first,second)
+where first and second are just the images in question.
+'''
+
+import numpy as np
+from skimage.util import invert
+from skimage.measure import label
+from skimage.metrics import adapted_rand_error
+
+def labelmake(S):
+    inverted = invert(S)
+
+    # Remove the border (1% on each side), which contains disconnects
+    s = S.shape[0]
+    b = round(s*1/100)
+    cropped = inverted[b:s-b,b:s-b]
+
+    label_image = label(cropped, connectivity = 1)
+    return label_image
+
+def find_randerr(first,second):
+    first_label = labelmake(first) 
+    second_label = labelmake(second)
+    randerr,p,r = adapted_rand_error(image_true=first_label, image_test=second_label, ignore_labels=[0])
+    return randerr
