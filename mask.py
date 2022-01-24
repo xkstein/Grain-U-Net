97d6afbeba5754285db2571c7cd0751b29b80cdb
xkstein <jamie.k.eckstein@gmail.com>
2022-01-24 14:35:51 -0600

    Debugged mask

--- mask.py
+++ mask.py
@@ -1,4 +1,5 @@
 from skimage import io, measure, morphology
+import skimage as sk
 import numpy as np
 import os
 import pdb
@@ -13,31 +14,26 @@ def find_mask(img=None, thresh=200, close_width=8, verbose=False):
     img_label = measure.label(img_thresh, background=0)
     bound = np.concatenate((img_label[0,:], img_label[-1,:], 
                             img_label[:,0], img_label[:,-1]))
-    mask_grains = np.zeros((img.shape))
+    mask_grains = np.ones((img.shape))
     grains = np.unique(bound)
     for grain in grains:
-        if grain == 0:
-            continue
-        mask_grains[img_label == grain] = 1
+#        if grain == 0:
+#            continue
+        mask_grains[img_label == grain] = 0
 
-    mask = morphology.closing(mask_grains, np.ones((close_width, close_width)))
+    mask = morphology.binary_closing(mask_grains, np.ones((close_width, close_width)))
     
     if verbose == True:
         img_masked = img.copy()
         img_masked[mask == 1] = 0
         io.imshow(img_masked)
         io.show()
-#        fuse = np.zeros((img.shape + (3,)))
-#        fuse[:,:,0] = mask
-#        fuse[:,:,1] = img / np.max(img)
-#        io.imshow(fuse)
-#        io.show()
 
     return mask
 
 if __name__ == "__main__":
-    input_path = 'data/train/label'
-    mask_path = 'data/train/mask'
+    input_path = 'data/label'
+    mask_path = 'data/mask'
     
     img_names = np.sort(os.listdir(input_path))
     valid = np.core.defchararray.find(img_names, '.png')
@@ -46,8 +42,12 @@ if __name__ == "__main__":
     for img_name in img_names:
         img_path = input_path + '/' + img_name
         img = io.imread(img_path)
+        if len(img.shape) > 2:
+            if img.shape[2] == 3:
+                img = img[:,:,0]
+
         mask_img = find_mask(img)
-        pdb.set_trace()
+        mask_img = sk.img_as_ubyte(mask_img)
         mask_img_path = mask_path + '/' + img_name
         io.imsave(mask_img_path, mask_img)
 
