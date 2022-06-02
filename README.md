fc001155d8c488c5244d43aa13aa546cdf05462a
ExL <26552638+exlL1@users.noreply.github.com>
2022-06-02 13:30:22 -0400

    added main_hyper_search for parameter tuning

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-# Grain U-Net
+# Notes
+I'm still deciding if it's a good idea to have data in this code base. I tend to keep code and data on different platforms so if I mess with it, and change code it won't mess with your local version. So for now, the data I'm using can be found here: https://drive.google.com/drive/folders/1DqZTwz4W53z-whbqt3C8YNUVCFEnQFn0?usp=sharing
 
-U-Net for detecting polycrystalline grain boundaries.
+Please let me know how you want to set up data sharing. Maybe each time we expand upon the set dramatically we should add a folder containing that data to the group drive? The benefits of this is that we could be looking at the data modularly, the downside is that the code isn't well suited for that (though thats something we could change).
 
-U-Net implementation we built off: https://github.com/zhixuhao/unet
+Original implementation: https://github.com/zhixuhao/unet
 
-To run the files in `scripts/` correctly, it is recommended that you use a python virtual machine and install src as a package. For example, this could mean setting up a virtual enviroment with the `python -m venv venv/` and then running `pip3 install -e .` in the root project directory.
+My current setup is to run this in a python virtual machine and install src as a package. To do the latter (which is required to access the files in src from `scripts/`), run `pip3 install -e .` in the main directory. This might be an overcomplication, but I'm trying it out for organization.
+
+Also, I'm adding docstrings to most of the functions. You can just read them in the file, but you can also see them by importing the function or module and then running `help(<function or module name>)` in python.
---
+++ src/unet_hypersearch.py
@@ -0,0 +1,85 @@
+'''DO NOT WORK AS IMPORTS! '''
+import numpy as np 
+import os
+from numpy.lib.function_base import append
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from tensorflow import keras
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+#from keras import backend as keras
+import tensorflow as tf
+import pdb
+
+def get_unet_hyper(pretrained_weights = None,input_size = (256,256,1), dropout_rate = 0.5):
+    inputs = Input(input_size)
+
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+
+    drop4 = Dropout(dropout_rate)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    drop5 = Dropout(dropout_rate)(conv5)
+
+    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+    merge6 = concatenate([drop4,up6], axis = 3)
+
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+    merge7 = concatenate([conv3,up7], axis = 3)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+    merge8 = concatenate([conv2,up8], axis = 3)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+    merge9 = concatenate([conv1,up9], axis = 3)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+
+    model = Model(inputs = inputs, outputs = conv10)
+
+    #model.summary()
+
+    if(pretrained_weights):
+    	model.load_weights(pretrained_weights)
+
+    return model
+
+def fit_with(dropout_rate, lr, epochs, batch_size, verbose=1):
+    model = get_unet_hyper(dropout_rate=dropout_rate)
+
+    opt = keras.optimizers.Adam(learning_rate = lr)
+    model.compile(optimizer = opt, loss='binary_crossentropy', metrics = ['accuracy'])
+
+    model.fit(x=train_gen, steps_per_epoch=len(train_gen), epochs=epochs, batch_size=batch_size, verbose=verbose)
+
+    #add in test set
+    score=model.evaluate(valid_gen , steps=len(valid_gen), verbose=0)
+
+    print('Test loss: ', score[0])
+    print('Test accuracy: ', score[1])
+
+    return score[1]
